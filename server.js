7dc40aa2efd3912a486c7a0fd4048fc4b809180f
Theo Goulart <theo_goulart@hotmail.com>
2017-03-24 03:46:42 +0000

    modify natural date response format

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var express = require('express')
 var app = express()
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+
 
 app.get('/:str', function (req, res) {
   
   var date = new Date(req.params.str)
   var result = {}
   
+  var strDate = months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear()
+  
   result.unix = date.getTime()
-  result.natural = date.toDateString()
+  result.natural = strDate
   
   res.send(JSON.stringify(result))
   
