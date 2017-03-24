54ccf5c2b7985ed0d2c69ae4db8de02cd4fed38c
Theo Goulart <theo_goulart@hotmail.com>
2017-03-24 02:52:48 +0000

    add get request handler

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:str', function (req, res) {
+  
+  var date = new Date(req.params.str)
+  var result = {}
+  
+  result.unix = date.getTime()
+  result.natural = date.toDateString()
+  
+  res.send(JSON.stringify(result))
+  
 })
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+app.listen(8080)
